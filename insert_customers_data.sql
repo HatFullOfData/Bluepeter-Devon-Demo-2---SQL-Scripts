e4750faa5e15ac58aa9f6708ff8cef94d0b89040
Hat Full Of Data <HFOD@graham-brown.co.uk>
2026-03-03 11:10:44 +0000

    Update insert_customers_data.sql with new customer data

---
+++ insert_customers_data.sql
@@ -0,0 +1,50 @@
+-- Insert Customer Data for Party Products Business
+-- Customers include restaurants, party planners, and event organizers
+
+-- Restaurants
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('The Ivy House', 'Sarah Mitchell', 'sarah@ivyhouse.co.uk', '020-555-0101', 'www.ivyhouse.co.uk', '45 High Street', 'London', 'W1A 1AA', 'UK', 'GB123456001', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Zetland Arms', 'James Chen', 'james@zetlandarms.co.uk', '0121-555-0102', 'www.zetlandarms.co.uk', '78 Canal Road', 'Birmingham', 'B1 2AA', 'UK', 'GB123456002', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Dishoom Manchester', 'Emma Thompson', 'emma@dishoom.com', '0161-555-0103', 'www.dishoom.com', '23 Market Square', 'Manchester', 'M1 3BB', 'UK', 'GB123456003', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Loch Fyne Restaurant', 'Marco Rossi', 'marco@lochfyne.com', '0117-555-0104', 'www.lochfyne.com', '56 Park Lane', 'Bristol', 'BS1 4CC', 'UK', 'GB123456004', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('The Kitchin', 'Rachel Green', 'rachel@thekitchin.com', '0131-555-0105', 'www.thekitchin.com', '89 Ocean Drive', 'Edinburgh', 'EH1 5DD', 'UK', 'GB123456005', GETDATE());
+
+-- Party Planners
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Banana Split Events', 'Sophie Anderson', 'sophie@bananasplit.co.uk', '020-555-0201', 'www.bananasplit.co.uk', '12 Party Plaza', 'London', 'SW1A 2EE', 'UK', 'GB123456006', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Frog & Princess Parties', 'David Wilson', 'david@frogprincess.co.uk', '0121-555-0202', 'www.frogprincess.co.uk', '34 Event Avenue', 'Birmingham', 'B2 3FF', 'UK', 'GB123456007', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Bentley & Co Events', 'Lucy Parker', 'lucy@bentleyevents.co.uk', '0161-555-0203', 'www.bentleyevents.co.uk', '67 Celebration Street', 'Manchester', 'M2 4GG', 'UK', 'GB123456008', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Pearl & Bloom Planning', 'Michael Brown', 'michael@pearlandbloom.co.uk', '0117-555-0204', 'www.pearlandbloom.co.uk', '90 Festival Road', 'Bristol', 'BS2 5HH', 'UK', 'GB123456009', GETDATE());
+
+-- Event Organizers
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Clive Mason Associates', 'Amanda White', 'amanda@clivemason.co.uk', '020-555-0301', 'www.clivemason.co.uk', '15 Conference Way', 'London', 'EC1A 3JJ', 'UK', 'GB123456010', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Haymarket Group', 'Thomas Davies', 'thomas@haymarketgroup.co.uk', '0121-555-0302', 'www.haymarketgroup.co.uk', '45 Business Park', 'Birmingham', 'B3 6KK', 'UK', 'GB123456011', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Upper Street Events', 'Jennifer Lee', 'jennifer@upperstreet.co.uk', '0161-555-0303', 'www.upperstreet.co.uk', '78 Gathering Green', 'Manchester', 'M3 7LL', 'UK', 'GB123456012', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Bluebird Corporate Events', 'Robert Taylor', 'robert@bluebirdcorp.co.uk', '0117-555-0304', 'www.bluebirdcorp.co.uk', '23 Venue Street', 'Bristol', 'BS3 8MM', 'UK', 'GB123456013', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Finch & Rose Events', 'Victoria Scott', 'victoria@finchrose.co.uk', '0131-555-0305', 'www.finchrose.co.uk', '56 Assembly Avenue', 'Edinburgh', 'EH2 9NN', 'UK', 'GB123456014', GETDATE());
+
+INSERT INTO Customer (CompanyName, ContactPerson, Email, Phone, Website, Address, City, PostalCode, Country, TaxID, CreatedDate)
+VALUES ('Albion Event Management', 'Oliver Harris', 'oliver@albionevents.co.uk', '020-555-0306', 'www.albionevents.co.uk', '89 Gathering Grove', 'London', 'N1 8PP', 'UK', 'GB123456015', GETDATE());
